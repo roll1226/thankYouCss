8a19f00718919385b1c649b000d9a1c6b06b56f2
roll1226 <tomo_yasu_4724@icloud.com>
2024-01-03 18:04:19 +0900

    最初のアニメーション作成途中

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,3 +3,7 @@ import '@/scss/style.scss'
 window.addEventListener('load', () => {
   console.log('ロード完了')
 })
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="start_1"></div>
+`
