000e6f8a77102516a908270193feef1fa0a5b4dc
roll1226 <tomo_yasu_4724@icloud.com>
2024-01-07 20:14:42 +0900

    サークルアニメーション追加完了

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,5 +8,9 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="animation_1">
     <div class="animation_1--circle_1"></div>
     <div class="animation_1--circle_2"></div>
+    <div class="animation_1--circle_3"></div>
+    <div class="animation_1--bar_1"></div>
+    <div class="animation_1--bar_2"></div>
+    <div class="animation_1--bar_3"></div>
   </div>
 `
