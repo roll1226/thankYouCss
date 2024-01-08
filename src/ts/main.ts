07700283603e0afffddbfe08246c3346763b8019
roll1226 <tomo_yasu_4724@icloud.com>
2024-01-08 17:50:39 +0900

    アニメーション一コマ目完成

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,11 +6,14 @@ window.addEventListener('load', () => {
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="animation_1">
-    <div class="animation_1--circle_1"></div>
-    <div class="animation_1--circle_2"></div>
-    <div class="animation_1--circle_3"></div>
-    <div class="animation_1--bar_1"></div>
-    <div class="animation_1--bar_2"></div>
-    <div class="animation_1--bar_3"></div>
+    <div class="animation_1--wrap">
+      <div class="animation_1--circle animation_1--circle_1"></div>
+      <div class="animation_1--circle animation_1--circle_2"></div>
+      <div class="animation_1--circle animation_1--circle_3"></div>
+      <div class="animation_1--bar animation_1--bar_1"></div>
+      <div class="animation_1--bar animation_1--bar_2"></div>
+      <div class="animation_1--bar animation_1--bar_3"></div>
+      <div class="animation_1--text">約</div>
+    </div>
   </div>
 `
