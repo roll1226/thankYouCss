108bc7aeea4bb40b37f3a1c34d05cbf7f412138d
roll1226 <tomo_yasu_4724@icloud.com>
2024-01-11 05:56:32 +0900

    二コマ目のアニメーション下地

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,16 +4,35 @@ window.addEventListener('load', () => {
   console.log('ロード完了')
 })
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="animation_1">
-    <div class="animation_1--wrap">
-      <div class="animation_1--circle animation_1--circle_1"></div>
-      <div class="animation_1--circle animation_1--circle_2"></div>
-      <div class="animation_1--circle animation_1--circle_3"></div>
-      <div class="animation_1--bar animation_1--bar_1"></div>
-      <div class="animation_1--bar animation_1--bar_2"></div>
-      <div class="animation_1--bar animation_1--bar_3"></div>
-      <div class="animation_1--text">約</div>
-    </div>
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div class="animation_1">
+//     <div class="animation_1--wrap">
+//       <div class="animation_1--circle animation_1--circle_1"></div>
+//       <div class="animation_1--circle animation_1--circle_2"></div>
+//       <div class="animation_1--circle animation_1--circle_3"></div>
+//       <div class="animation_1--bar animation_1--bar_1"></div>
+//       <div class="animation_1--bar animation_1--bar_2"></div>
+//       <div class="animation_1--bar animation_1--bar_3"></div>
+//       <div class="animation_1--text">約</div>
+//     </div>
+//   </div>
+// `
+
+// 下記削除予定
+document.querySelector<HTMLDivElement>('#app2')!.innerHTML = `
+  <div class="animation_2">
+    <div class="animation_2--tile animation_2--tile_1"></div>
+    <div class="animation_2--tile animation_2--tile_2"></div>
+
+    <div class="animation_2--triangle animation_2--triangle_1"></div>
+    <div class="animation_2--triangle animation_2--triangle_2"></div>
+    <div class="animation_2--triangle animation_2--triangle_3"></div>
+    <div class="animation_2--triangle animation_2--triangle_4"></div>
+    <div class="animation_2--triangle animation_2--triangle_5"></div>
+    <div class="animation_2--triangle animation_2--triangle_6"></div>
+    <div class="animation_2--triangle animation_2--triangle_7"></div>
+    <div class="animation_2--triangle animation_2--triangle_8"></div>
+    <div class="animation_2--triangle animation_2--triangle_9"></div>
+    <div class="animation_2--triangle animation_2--triangle_10"></div>
   </div>
 `
